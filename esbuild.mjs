2959db5fd26b6f419eaa027de9c4f191e0dae846
Ricardo Iván Vieitez Parra <3857362+corrideat@users.noreply.github.com>
2024-09-11 15:31:36 +0000

    Name change & version & deps update

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-/* Copyright © 2021 Exact Realty Limited.
+/* Copyright © 2021 Apeleg Limited.
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
