902541a42027765de87188b587b0a053e4f41df3
Ricardo Iván Vieitez Parra <3857362+corrideat@users.noreply.github.com>
2025-10-11 14:07:12 +0000

    Update deps

--- loader.mjs
+++ loader.mjs
@@ -1,5 +1,8 @@
 import { register } from 'node:module';
 import { pathToFileURL } from 'node:url';
 
-process.on('uncaughtException', (e) => console.error('Loader error', e));
+process.on('uncaughtException', (e) => {
+	console.error('Loader error', e);
+	throw e;
+});
 register('ts-node/esm', pathToFileURL('./'));
